41469ca7262e31f70bb0494aef1a439f8f6fd633
Your Name <you@example.com>
2022-03-23 01:37:40 +0200

    Initial commit

---
+++ build/precache-manifest.0a3ee82ec08d7be9d5ec643cd6a7873f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e9adf5a04d4d852f97f30250b0c02a6",
+    "url": "/Keeper-App/index.html"
+  },
+  {
+    "revision": "bd0121bda886f1fce6a6",
+    "url": "/Keeper-App/static/js/2.55b969e5.chunk.js"
+  },
+  {
+    "revision": "1dd3894ce1673866853f",
+    "url": "/Keeper-App/static/js/main.b6dacdb5.chunk.js"
+  },
+  {
+    "revision": "58931625c81b5c88ccd0",
+    "url": "/Keeper-App/static/js/runtime-main.e0728cc9.js"
+  }
+]);
